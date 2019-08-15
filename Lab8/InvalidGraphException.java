68013596ba9bae4491bb4f125a411e6deab40984
Benjamin Rees <br7g18@soton.ac.uk>
2019-08-15 22:59:37 +0100

    Invalid Graph Exception

---
+++ Lab8/InvalidGraphException.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * <p>
+ * A checked exception indicating an invalid graph.
+ * </p>
+ * 
+ * @author htson
+ */
+public class InvalidGraphException extends Exception {
+
+	/**
+	 * The default constructor.
+	 */
+	public InvalidGraphException() {
+		super();
+	}
+}
